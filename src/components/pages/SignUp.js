23c0224be05b6518c96ed2a8a03c9e7bab10df2b
Jeff2hu <lovezhu661991@gmail.com>
2022-08-26 01:54:20 +0800

    alert

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { api_register } from '../store/api/API';
 
 const SignUp = () => {
+
+  const [ message,setMessage ] = useState("")
 
   const navigate = useNavigate();
   const { register, handleSubmit, watch, formState:{ errors } } = useForm();
   const onSubmit = ({email,password,nickName}) => {
-    console.log(email,password,nickName)
-    navigate("/")
+    api_register("users",{
+      "email":email,
+      "nickName":nickName,
+      "password":password
+    })
+    .then(res=>{
+      if(res.status===201){
+        setTimeout(()=>{
+          navigate("/")
+        },1500)
+      }
+      return res.json()
+    })
+    .then(res=>{
+      setMessage(res.message)
+    })
+    .catch(error=>{
+      setMessage(error.error)
+    })
   }
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setMessage("")
+    },2000)
+  },[message])
 
   return (
     <div id="signUpPage" className="bg-yellow">
@@ -18,91 +44,90 @@ const SignUp = () => {
             <a><img className="logoImg" src="https://upload.cc/i1/2022/03/23/rhefZ3.png" alt=""/></a>
             <img className="d-m-n" src="https://upload.cc/i1/2022/03/23/tj3Bdk.png" alt="workImg"/>
         </div>
-        <div>
-            <form 
-              className="formControls"
-              onSubmit={handleSubmit(onSubmit)}>
-                <h2 className="formControls_txt">註冊帳號</h2>
-                <label className="formControls_label" htmlFor='email'>Email
-                <p className='noValue'>{errors.email && errors.email?.message}</p>
-                </label>
-                <input 
-                  className="formControls_input"
-                  type="text"
-                  id="email"
-                  placeholder="請輸入信箱"
-                  {...register("email",{ 
-                    required: {
-                      value : true, 
-                      message: "此欄位不可留空"
-                    },
-                    pattern: {
-                      value : /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-                      message : "不符合Email格式"
+          <form 
+            className="formControls"
+            onSubmit={handleSubmit(onSubmit)}>
+              <h2 className="formControls_txt">註冊帳號</h2>
+              <label className="formControls_label" htmlFor='email'>Email
+              <p className='noValue'>{errors.email && errors.email?.message}</p>
+              </label>
+              <input 
+                className="formControls_input"
+                type="text"
+                id="email"
+                placeholder="請輸入信箱"
+                {...register("email",{ 
+                  required: {
+                    value : true, 
+                    message: "此欄位不可留空"
+                  },
+                  pattern: {
+                    value : /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                    message : "不符合Email格式"
+                  }
+                })}
+              />
+              <label className="formControls_label" htmlFor='nickName'>Nick Name
+              <p className='noValue'>{errors.nickName && errors.nickName?.message}</p>
+              </label>
+              <input 
+                className="formControls_input"
+                type="text"
+                id="nickName"
+                placeholder="請輸入暱稱"
+                {...register("nickName",{ 
+                  required: {
+                    value : true, 
+                    message: "此欄位不可留空"
+                  }
+                })}
+              />
+              <label className="formControls_label" htmlFor='password'>Password
+              <p className='noValue'>{errors.password && errors.password?.message}</p>
+              </label>
+              <input 
+                className="formControls_input"
+                type="password"
+                id="password"
+                placeholder="請輸入密碼"
+                {...register("password",{ 
+                  required:{
+                    value:true,
+                    message:"此欄位不可留空"
+                  }, 
+                  minLength:{
+                    value:6,
+                    message:"密碼長度至少6位數"
+                  }
+                })}
+              />
+              <label className="formControls_label" htmlFor='checkPassword'>Check Password
+              <p className='noValue'>{errors.checkPassword?.message}</p>
+              </label>
+              <input 
+                className="formControls_input"
+                type="password"
+                id="checkPassword"
+                placeholder="請再次輸入密碼"
+                {...register("checkPassword",{ 
+                  required:{
+                    value: true ,
+                    message: "此欄位不可留空"
+                  },
+                  validate: (value) => {
+                    if (watch('password') !== value) {
+                      return "兩次密碼輸入不同";
                     }
-                  })}
-                />
-                <label className="formControls_label" htmlFor='nickName'>Nick Name
-                <p className='noValue'>{errors.nickName && errors.nickName?.message}</p>
-                </label>
-                <input 
-                  className="formControls_input"
-                  type="text"
-                  id="nickName"
-                  placeholder="請輸入暱稱"
-                  {...register("nickName",{ 
-                    required: {
-                      value : true, 
-                      message: "此欄位不可留空"
-                    }
-                  })}
-                />
-                <label className="formControls_label" htmlFor='password'>Password
-                <p className='noValue'>{errors.password && errors.password?.message}</p>
-                </label>
-                <input 
-                  className="formControls_input"
-                  type="password"
-                  id="password"
-                  placeholder="請輸入密碼"
-                  {...register("password",{ 
-                    required:{
-                      value:true,
-                      message:"此欄位不可留空"
-                    }, 
-                    minLength:{
-                      value:6,
-                      message:"密碼長度至少6位數"
-                    }
-                  })}
-                />
-                <label className="formControls_label" htmlFor='checkPassword'>Check Password
-                <p className='noValue'>{errors.checkPassword?.message}</p>
-                </label>
-                <input 
-                  className="formControls_input"
-                  type="password"
-                  id="checkPassword"
-                  placeholder="請再次輸入密碼"
-                  {...register("checkPassword",{ 
-                    required:{
-                      value: true ,
-                      message: "此欄位不可留空"
-                    },
-                    validate: (value) => {
-                      if (watch('password') !== value) {
-                        return "兩次密碼輸入不同";
-                      }
-                  }})}
-                />
-                <input
-                  className="formControls_btnSubmit" 
-                  type="submit" 
-                  value="註冊帳號"
-                />
-                <Link className="formControls_btnLink" to="/">登入</Link>
-            </form>
-        </div>
+                }})}
+              />
+              <input
+                className="formControls_btnSubmit" 
+                type="submit" 
+                value="註冊帳號"
+              />
+              <Link className="formControls_btnLink" to="/">登入</Link>
+          </form>
+          <div className={message?"alertMessage_container":""}><p>{message}</p></div>
         </div>
     </div>
   )
--- src/components/store/api/API.js
+++ src/components/store/api/API.js
@@ -1,5 +1,17 @@
 const API_URL = "https://todoo.5xcamp.us/";
 
+export const api_register = async(url,data) => {
+  return await fetch(API_URL+url,{
+    method:"POST",
+    headers:{
+      'content-type': 'application/json'
+    },
+    body:JSON.stringify({
+      "user":data
+    })
+  })
+}
+
 // const api_addTodo = async(url,data) => {
 //   const addTodo = await fetch(API_URL+url,{
 //     method:"POST",
